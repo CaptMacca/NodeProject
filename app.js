d37b10c103c2a9f0930099c8dcccccb9cf7c326f
CaptMacca <mackysm@gmail.com>
2018-04-05 20:37:17 +0800

    Added bower and reworked bootstrap and jquery

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static("bower_components"));
+
+app.get('/', function(req, res){
+    res.send('Aloha World!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing!');
+});
+
+app.listen(port, function(err) {
+    console.log('The server is running on port: ' + port);
+});
