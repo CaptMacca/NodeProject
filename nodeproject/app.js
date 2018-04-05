b0f0141b4d9caac8d5e13595242d12fedbeb2fb7
CaptMacca <mackysm@gmail.com>
2018-04-05 16:17:06 +0800

    Added express and some basic routing

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,18 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var port = 5000;
+
+app.get('/', function(req, res){
+    res.send('Aloha World!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing!');
+});
+
+app.listen(port, function(err) {
+    console.log('The server is running on port: ' + port);
+});
